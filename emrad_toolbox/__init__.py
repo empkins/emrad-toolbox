6a37c4732a984a5a7038bbb389d45b52df51dedd
Simon Meske <simon.h.meske@fau.de>
2024-01-29 15:34:03 +0100

    Fehler in __init__.py behoben -> RadarPreporcessor in emrad_toolbox/__innit.py importiert.

--- emrad_toolbox/__init__.py
+++ emrad_toolbox/__init__.py
@@ -1,3 +1,5 @@
 """emrad_toolbox - A collection of tools for EMRaD."""
+from emrad_toolbox.radar_preprocessing.radar import RadarPreprocessor
 
+__all__ = ["RadarPreprocessor"]
 __version__ = "0.1.0"
--- emrad_toolbox/radar_preprocessing/__init__.py
+++ emrad_toolbox/radar_preprocessing/__init__.py
@@ -1,4 +1,1 @@
 """emrad_toolbox Radar - A collection of tools to handle Radar data."""
-from emrad_toolbox.radar_preprocessing.radar import RadarPreprocessor
-
-__all__ = ["RadarPreprocessor"]
